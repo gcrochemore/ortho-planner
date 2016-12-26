6d3b530692cccc27d4d2a7b23d997eb410af295c
Guillaume Crochemore <guillaume.crochemore@gmail.com>
2016-12-26 18:29:01 +0100

    Chnage form scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :patients
-  resources :patients
   devise_for :users, controllers: { confirmations: 'confirmations' }
   root to: "application#index"
   # The priority is based upon order of creation: first created -> highest priority.
