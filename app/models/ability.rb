69db40c932351008ddf34fb3e9420da46c1769a3
Guillaume Crochemore <guillaume.crochemore@gmail.com>
2016-12-28 22:49:42 +0100

    Show role in user view

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
     #
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
-      can :read, Pathology
+      can :manage, :all
     else
       can :read, :all
     end
