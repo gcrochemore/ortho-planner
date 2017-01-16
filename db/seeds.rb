a314df4cfa410ee59cc336495a866c8d057800fb
Guillaume Crochemore <guillaume.crochemore@gmail.com>
2017-01-16 22:26:22 +0100

    Improve seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-user = User.create( email: "secretaire@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner")
-user.save
-user.add_role :secretary
-
-user = User.create( email: "waiting-list-manager@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner")
-user.save
-user.add_role :waiting_list_manager
-
-user = User.create( email: "orthophoniste@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner")
-user.save
-user.add_role :practitioner
-
-user = User.create( email: "repository-manager@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner")
-user.save
-user.add_role :repository_manager
-
-user = User.create( email: "admin@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner")
-user.save
-user.add_role :admin
-
 pathology = Pathology.create(name: "Voix")
 pathology.save
 pathology = Pathology.create(name: "Langage écrit")
@@ -53,7 +32,6 @@ act = Act.create(name: "Bilan de la déglutition et des fonctions oro-myo foncti
 act.save
 act = Act.create(name: "Bilan de renouvellement", AMO: 11.2)
 act.save
-
 act = Act.create(name: "Bilan de la phonation", AMO: 24)
 act.save
 act = Act.create(name: "Bilan du langage oral et/ou bilan d’aptitudes à l’acquisition du langage écrit", AMO: 24)
@@ -62,23 +40,18 @@ act = Act.create(name: "Bilan du langage écrit", AMO: 24)
 act.save
 act = Act.create(name: "Bilan de la dyscalculie et des troubles du raisonnement logico- mathématique", AMO: 24)
 act.save
-
 act = Act.create(name: "Bilan de renouvellement", AMO: 16.8)
 act.save
-
 act = Act.create(name: "Bilan des troubles d’origine neurologique", AMO: 30)
 act.save
 act = Act.create(name: "Bilan du bégaiement", AMO: 30)
 act.save
 act = Act.create(name: "Bilan du langage dans le cadre des handicaps moteurs, sensoriels ou mentaux (inclus surdité, IMC, autisme, maladies génétiques)", AMO: 30)
 act.save
-
 act = Act.create(name: "Bilan de renouvellement", AMO: 21)
 act.save
-
 act = Act.create(name: "Rééducation des troubles d’articulation isolés chez des personnes ne présentant pas d’affectation neurologique", AMO: 5.1)
 act.save
-
 act = Act.create(name: "Rééducation de la déglutition atypique", AMO: 8)
 act.save
 act = Act.create(name: "Rééducation des troubles de l'articulation liés à des déficiences perceptives", AMO: 8)
@@ -87,10 +60,8 @@ act = Act.create(name: "Rééducation des troubles de l'articulation liés à de
 act.save
 act = Act.create(name: "Rééducation vélo-tubo-tympanique", AMO: 8)
 act.save
-
 act = Act.create(name: "Rééducation des troubles de l’écriture", AMO: 10)
 act.save
-
 act = Act.create(name: "Rééducation des troubles de l’écriture", AMO: 10)
 act.save
 act = Act.create(name: "Rééducation des pathologies du langage écrit: lecture et/ou orthographe", AMO: 10.1)
@@ -99,17 +70,14 @@ act = Act.create(name: "Rééducation des troubles du calcul et du raisonnement 
 act.save
 act = Act.create(name: "Rééducation des anomalies des fonctions oro faciales entraînant des troubles de l’articulation et de la parole", AMO: 10.3)
 act.save
-
 act = Act.create(name: "Rééducation des dysarthries neurologiques", AMO: 11)
 act.save
 act = Act.create(name: "Rééducation des dysphagies chez l’adulte et l’enfant", AMO: 11)
 act.save
-
 act = Act.create(name: "Éducation à l'utilisation des prothèses phonatoires", AMO: 11.1)
 act.save
 act = Act.create(name: "Rééducation des troubles de la voie d’origine organique ou fonctionnelle", AMO: 11.4)
 act.save
-
 act = Act.create(name: "Éducation à l'acquisition et à l'utilisation de la voix oro-œsophagienne et/ou trachéo-œsophagienne", AMO: 11.2)
 act.save
 act = Act.create(name: "Rééducation du mouvement paradoxal d’adduction des cordes vocales à l’inspiration", AMO: 11.3)
@@ -124,14 +92,12 @@ act = Act.create(name: "Éducation précoce au langage dans les handicaps de l'e
 act.save
 act = Act.create(name: "Éducation ou rééducation du langage dans les handicaps de l'enfant de type sensoriel, moteur, mental", AMO: 13.5)
 act.save
-
 act = Act.create(name: "Éducation ou rééducation du langage dans le cadre de l’autisme", AMO: 13.8)
 act.save
 act = Act.create(name: "Éducation ou rééducation du langage dans le cadre des maladies génétiques", AMO: 13.8)
 act.save
 act = Act.create(name: "Éducation ou rééducation du langage dans le cadre de l’infirmité motrice cérébrale", AMO: 13.8)
 act.save
-
 act = Act.create(name: "Rééducation des dysphasies", AMO: 14)
 act.save
 act = Act.create(name: "Rééducation du langage dans les aphasies", AMO: 15.6)
@@ -149,21 +115,10 @@ act.save
 
 health_place_type = HealthPlaceType.create(name: "Institut médico-éducatif")
 health_place_type.save
-health_place = HealthPlace.create(name: "IME de Arques-la-bataille", health_place_type: health_place_type)
-health_place.save
-
 health_place_type = HealthPlaceType.create(name: "Centre médico-psychologique")
 health_place_type.save
-health_place = HealthPlace.create(name: "CMP de Dieppe", health_place_type: health_place_type)
-health_place.save
-
 health_place_type = HealthPlaceType.create(name: "Cabinet d'orthophonie")
 health_place_type.save
-
-health_place = HealthPlace.create(name: "Cabinet de Tôtes", health_place_type: health_place_type)
-health_place.save
-address = Address.create(name: "Cabinet de Tôtes", addressable: health_place)
-address.save
 
 practitioner_profession = PractitionerProfession.create(name: "Psychologue")
 practitioner_profession.save
@@ -173,19 +128,6 @@ practitioner_profession = PractitionerProfession.create(name: "Infirmière")
 practitioner_profession.save
 practitioner_profession = PractitionerProfession.create(name: "Orthophoniste")
 practitioner_profession.save
-
-practitioner = Practitioner.create(first_name: "Perrine", last_name: "Clatz", health_place: health_place, practitioner_profession: practitioner_profession);
-practitioner.save 
-practitioner = Practitioner.create(first_name: "Jeanne", last_name: "Gazan", health_place: health_place, practitioner_profession: practitioner_profession);
-practitioner.save 
-practitioner = Practitioner.create(first_name: "Louise", last_name: "Leporcq", health_place: health_place, practitioner_profession: practitioner_profession);
-practitioner.save 
-practitioner = Practitioner.create(first_name: "Anaëlle", last_name: "Etienne", health_place: health_place, practitioner_profession: practitioner_profession);
-practitioner.save 
-practitioner = Practitioner.create(first_name: "Monique", last_name: "Briand", health_place: health_place, practitioner_profession: practitioner_profession);
-practitioner.save 
-practitioner = Practitioner.create(first_name: "Emmanuelle", last_name: "Laville", health_place: health_place, practitioner_profession: practitioner_profession);
-practitioner.save 
 
 interaction_type = InteractionType.create(name: "Appel téléphonique", glyphicon: "glyphicon glyphicon-earphone")
 interaction_type.save
@@ -201,6 +143,8 @@ interaction_type.save
 interaction_object = InteractionObject.create(name: "Inscrire sur la liste d'attente")
 interaction_type.save
 interaction_object = InteractionObject.create(name: "Relance inscription sur liste d'attente")
+interaction_type.save
+interaction_object = InteractionObject.create(name: "Proposition de prise en charge")
 interaction_type.save
 
 
@@ -239,19 +183,8 @@ school_type = SchoolType.create(name: "Ecole maternelle")
 school_type.save
 school_type = SchoolType.create(name: "Ecole élémentaire")
 school_type.save
-
-school = School.create(name: "Ecole elementaire publique d'Ambrumesnil", school_type: school_type)
-school.save
-address = Address.create(name: "Ecole elementaire publique d'Ambrumesnil", street_name: "Rue de l'ancienne mare", postal_code: "76550", city: "Ambrumesnil", addressable: school)
-address.save
-
 school_type = SchoolType.create(name: "Collège")
 school_type.save
-
-school = School.create(name: "Collège de Luneray", school_type: school_type)
-school.save
-
-
 school_type = SchoolType.create(name: "Lycée")
 school_type.save
 school_type = SchoolType.create(name: "Centre de fomation d'apprentis")
@@ -295,6 +228,37 @@ school_year = SchoolYear.create(begin_date: "01/09/2010", end_date: "01/07/2011"
 school_year.save
 school_year = SchoolYear.create(begin_date: "01/09/2009", end_date: "01/07/2010")
 school_year.save
+
+
+#
+#
+# Seed de démonstration
+#
+#
+
+
+
+user = User.create( email: "secretaire@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner")
+user.save
+user.add_role :secretary
+
+user = User.create( email: "waiting-list-manager@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner", practitioner: practitioner)
+user.save
+user.add_role :waiting_list_manager
+
+user = User.create( email: "orthophoniste@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner")
+user.save
+user.add_role :practitioner
+
+user = User.create( email: "repository-manager@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner")
+user.save
+user.add_role :repository_manager
+
+user = User.create( email: "admin@ortho-planner.com", password: "ortho-planner", password_confirmation: "ortho-planner")
+user.save
+user.add_role :admin
+
+
 
 patient = Patient.create(last_name: "Dupond",  first_name: "Ludovic", is_male: true, birth_date: "01/01/2010", priority: false, comments: "J'ai demandé un bilan ORL. Difficultés en lecture, ne comprend pas les consignes")
 patient.save
@@ -366,3 +330,22 @@ contact_information = ContactInformation.create(contactable: patient, value: "+3
 contact_information.save
 schooling = Schooling.create(patient: patient, school_level: school_level_ce2, school_year: school_year)
 schooling.save
+
+
+school = School.create(name: "Ecole elementaire publique d'Ambrumesnil", school_type: school_type)
+school.save
+address = Address.create(name: "Ecole elementaire publique d'Ambrumesnil", street_name: "Rue de l'ancienne mare", postal_code: "76550", city: "Ambrumesnil", addressable: school)
+address.save
+school = School.create(name: "Collège de Luneray", school_type: school_type)
+school.save
+
+
+health_place = HealthPlace.create(name: "IME de Arques-la-bataille", health_place_type: health_place_type)
+health_place.save
+health_place = HealthPlace.create(name: "CMP de Dieppe", health_place_type: health_place_type)
+health_place.save
+health_place = HealthPlace.create(name: "Cabinet de Tôtes", health_place_type: health_place_type)
+health_place.save
+address = Address.create(name: "Cabinet de Tôtes", addressable: health_place)
+address.save
+
